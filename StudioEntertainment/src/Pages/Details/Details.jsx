46934f237e02692c1890e6bd9aab3494f1710128
mosa <mosazaben1@gmail.com>
2025-06-01 21:24:29 +0300

    final resalt

--- StudioEntertainment/src/Pages/Details/Details.jsx
+++ StudioEntertainment/src/Pages/Details/Details.jsx
@@ -1,7 +1,10 @@
 import React, {useEffect, useState} from 'react';
 import {useParams} from "react-router-dom";
 import axios from "axios";
-import { FaRegClock } from "react-icons/fa";
+import {FaRegClock} from "react-icons/fa";
+import {CiCalendar} from "react-icons/ci";
+import './Details.css'
+
 
 const Details = () => {
     const [data, setData] = useState({});
@@ -18,29 +21,33 @@ const Details = () => {
 
     }, []);
     return (
-        <div>
+        <div className="details">
+            <div className="details-poster">
             <img src={data.posterUrl} alt=""/>
-            <div>
+            </div>
+            <div className="details-content">
                 <h1>{data.title}</h1>
-                <div className="genres">
+                <div className="genres-container" >
                     {
                         data.genres?.map(item => (
-                            <span key={item}>{item}</span>
+                            <span className="genres" key={item}>{item}</span>
                         ))
                     }
 
                 </div>
-                <p>{data.details?.description}</p>
-                <div>
-                    <span><FaRegClock /><p></p></span>
-                    <span><p></p></span>
+                <p className="desc">{data.details?.description}</p>
+                <div className="date-info">
+                    <span className="datee"><FaRegClock  className="icon"/>Release Date<p>{data.details?.releaseDate} </p></span>
+                    <span className="datee"><CiCalendar className="icon"/>duration <p>{data.details?.duration}</p></span>
 
                 </div>
                 <div>
-                    <span><p></p><p></p></span>
-                    <span><p></p><p></p></span>
-                    <span><p></p><p></p></span>
-                    <span><p></p><p></p></span>
+                    <span className="about"><p>Director:</p><p>{data.details?.director}</p></span>
+                    <span className="about"><p>Cast:</p><span>{data.details?.cast.map(item => (<span key={item}>{item}</span>
+                    ))}</span></span>
+                    <span className="about"><p>Language:</p><p>{data.details?.languages}</p></span>
+                    <span className="about"><p>Rating:</p><p>{data.details?.rating}</p></span>
+
                 </div>
 
             </div>
